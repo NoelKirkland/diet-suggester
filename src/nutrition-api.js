e5006600de5599420f87ba48b8828c47df83b6fc
Noel Kirkland <noelkirkland@gmail.com>
2020-07-09 16:45:57 -0700

    finalize edits
    Co-authored-by: Erich Richter<erich.richter@gmail.com>
    dquote> co-authored-by: Cody Fritz <clanalia55@gmail.com>
    dquote> co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- src/nutrition-api.js
+++ src/nutrition-api.js
@@ -1,6 +1,6 @@
 export async function getFood(food) {
     try {
-      let response = await fetch(`https://api.edamam.com/api/food-database/v2/parser?nutrition-type=logging&ingr=${food}&app_id=2b2666a9&app_key=8704a2c1fb5777a097e8e7f564118326`);
+      let response = await fetch(`https://api.edamam.com/api/food-database/v2/parser?nutrition-type=logging&ingr=${food}&app_id=${foodApiID}&app_key=${foodApiKey}`);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
